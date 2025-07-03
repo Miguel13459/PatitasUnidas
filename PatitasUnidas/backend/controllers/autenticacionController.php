5d03cd7318c5a85b6aacb9971c04be09c98114a6
Miguel13459 <al217857@alumnos.uacj.mx>
2025-07-03 00:08:22 -0600

    desarollo de la clase inicio de sesion y cerrar sesión

--- PatitasUnidas/backend/controllers/autenticacionController.php
+++ PatitasUnidas/backend/controllers/autenticacionController.php
@@ -1,9 +1,39 @@
 <?php
-    class CentroAdoptivo{
-        private bool $sesionIniciada;
+    require_once '../config/config.php';
+    class Autenticacion{
+        public bool $sesionIniciada;
+        private string $contrasenia;
+        private string $usuario;
+
+        public function __construct($Usuario, $Contrasenia) {
+            $this->usuario = $Usuario;
+            $this->contrasenia = $Contrasenia;
+        }
 
         public function validarCredenciales(){
-            
+            session_start();
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $usuario = $_POST['usuario'];
+                $contrasenia = $_POST['contrasenia'];
+
+                if ($conn->connect_error) {
+                die("Conexión fallida: " . $conn->connect_error);
+                }
+
+                $stmt = $conn->prepare("SELECT personal.usuario, personal.contrasenia FROM personal WHERE usuario = ? AND contrasenia = ?");
+                $stmt->bind_param("ss", $usuario, $contrasenia);
+                $stmt->execute();
+                $resultado = $stmt->get_result();
+
+                if ($resultado->num_rows == 1) {
+                    $usuario = $resultado->fetch_assoc();
+                    $_SESSION['rol'] = $usuario['rol'];
+                    $_SESSION['usuario'] = $usuario['usuario'];
+                    exit;
+                } else {
+                    echo "Correo o contraseña incorrectos.";
+                }
+            }
         }
     }    
 ?>
--- PatitasUnidas/backend/controllers/empleado.php
+++ PatitasUnidas/backend/controllers/empleado.php
@@ -1,5 +1,9 @@
 <?php
     require_once '../config/config.php';
+    require_once 'autenticacionController.php';
+    require_once '../models/centroAdoptivo.php';
+    require_once '../models/eventos.php';
+    require_once '../models/mascota.php';
 
     class Empleado{
         public int $idEmpleado;
@@ -8,39 +12,31 @@
         private int $tipoSucursal;
     
         public function iniciarSesion(){
-            session_start();
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                $usuario = $_POST['usuario'];
-                $contrasenia = $_POST['contrasenia'];
-
-                $conn = new mysqli($servidor, $usuario, $clave, $basededato);
-                if ($conn->connect_error) {
-                die("Conexión fallida: " . $conn->connect_error);
-                }
-
-                $stmt = $conn->prepare("SELECT * FROM personal WHERE usuario = ? AND contrasenia = ?");
-                $stmt->bind_param("ss", $usuario, $contrasenia);
-                $stmt->execute();
-                $resultado = $stmt->get_result();
-
-                if ($resultado->num_rows == 1) {
-                    $usuario = $resultado->fetch_assoc();
-                    $_SESSION['rol'] = $usuario['rol'];
-                    $_SESSION['usuario'] = $usuario['nombre'];
-                    header("Location: ../../frontend/src/pages/adopciones.php");
-                    exit;
-                } else {
-                    echo "Correo o contraseña incorrectos.";
-                }
+            $inicioDeSesion = new Autenticacion($usuario, $contrasenia);
+            if($inicioDeSesion){
+                header("Location: ../../frontend/src/pages/adopciones.php");
             }
         }
 
         public function cerrarSesion(){
-            
+            session_start();
+            session_destroy();
+            header("Location: ../../frontend/src/public/index.html");
         }
 
         public function crearMascota(){
+            session_start();
+            $visibilidadSitio = 1;
+
             
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $stmt = $conn->prepare("INSERT INTO mascota (nombre, especie, edad, sexo, tamanio, visibilidadSitio, descripcion, fotografia) VALUES (?, ?, ?, ?)");
+                $stmt->bind_param("siss", $_POST['nombre'], $_POST['edad'], $_POST['descripcion'], $_POST['imagen_url']);
+                $stmt->execute();
+                header("Location: adopciones.php");
+                exit;
+}
         }
 
         public function editarMascota(){
