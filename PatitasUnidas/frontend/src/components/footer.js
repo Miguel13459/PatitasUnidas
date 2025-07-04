0320e438022a05f71f8e815424260d76a7dd595e
208824 <scitlaliandrade11@gmail.com>
2025-07-03 19:51:44 -0600

    Banner abajo de las tarjetas y footer en js

---
+++ PatitasUnidas/frontend/src/components/footer.js
@@ -0,0 +1,15 @@
+document.getElementById("footer-placeholder").innerHTML = `
+  <footer class="footer">
+    <img src="/PatitasUnidas/frontend/src/assets/LogoPatiasUnidas.png" alt="Patitas Unidas" class="footer-logo">
+    <p class="footer-subtitulo">Dirección</p>
+    <p class="footer-direccion">
+      Calle Esperanza #214,<br>
+      Colonia Los Encinos, C.P. 32789, Ciudad Juárez, Chihuahua, México
+    </p>
+    <div class="footer-redes">
+      <a href="#" target="_blank"><i class="fab fa-facebook"></i></a>
+      <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+      <a href="#" target="_blank"><i class="fab fa-tiktok"></i></a>
+    </div>
+  </footer>
+`;
