e30c18795151529a91a2d8db009ab2e1456143dc
208824 <scitlaliandrade11@gmail.com>
2025-07-17 03:34:18 -0600

    Acomode estilos del footer que se quitaron

--- public/Formulario-Crear.php
+++ public/Formulario-Crear.php
@@ -18,6 +18,7 @@ if (!isset($_SESSION['usuario'])) {
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="CSS/estiloAdopcion.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
   <!-- Encabezado -->
